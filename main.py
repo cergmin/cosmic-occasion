17c86aee1fa56525b51e5bb80d7d19f96f704588
tainiikrab <60309219+tainiikrab@users.noreply.github.com>
2020-12-22 18:29:13 +1000

    Добавлено упраление мышью, добавлен прицел
    А также добавлена возможность скрыть курсор

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 from math import cos, sin, radians
 from settings import *
-from ray import Ray
 from world import World
 from player import Player
 from drawing import Drawing
@@ -12,22 +11,23 @@ if __name__ == '__main__':
 
     size = WIDTH, HEIGHT
     screen = pygame.display.set_mode(size)
-
     player = Player(100, 100)
     world = World([
         ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
         ['w', '.', '.', '.', '.', '.', '.', 'w'],
         ['w', '.', 'w', 'w', 'w', 'w', '.', 'w'],
-        ['w', '.', '.', '.', '.', 'w', '.', 'w'],
-        ['w', '.', '.', '.', '.', '.', '.', 'w'],
-        ['w', '.', '.', '.', '.', '.', '.', 'w'],
+        ['w', '.', 'w', '.', '.', 'w', '.', 'w'],
+        ['w', '.', 'w', '.', '.', 'w', '.', 'w'],
+        ['w', '.', 'w', '.', '.', 'w', '.', 'w'],
         ['w', '.', '.', '.', '.', '.', '.', 'w'],
         ['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w']
     ])
-    
+
     draw = Drawing(screen)
 
     running = True
+    is_cursor_event_odd = False
+    pygame.mouse.set_visible(False)
     clock = pygame.time.Clock()
     while running:
         tick = clock.tick() / 1000
@@ -53,9 +53,19 @@ if __name__ == '__main__':
         if keys[pygame.K_RIGHT]:
             player.vx += 80 * tick
 
+        rel_move = pygame.mouse.get_rel()
+        if rel_move[0] != 0:
+            if is_cursor_event_odd:
+                print(rel_move)
+                player.vx += rel_move[0] * (1 / SENSITIVITY)
+                pygame.mouse.set_pos(WIDTH // 2, HEIGHT // 2)
+            is_cursor_event_odd = not is_cursor_event_odd
+
         draw.background()
         draw.world(world, player)
         draw.fps(clock)
+
+        pygame.draw.rect(screen, (255, 0, 0), (WIDTH // 2, HEIGHT // 2, 10, 10))
 
         pygame.display.flip()
 
